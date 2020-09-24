4da0c58c1b8df3a85d02f7c80d12ecf4b6373472
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-09-24 22:31:04 +0300

    side bar function added

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,2 +1,18 @@
 import Glide from '@glidejs/glide';
 import '../../scss/components/card.scss';
+import '../../scss/components/sideBar.scss';
+
+const addButton = document.querySelector('.add-button');
+const sideBar = document.querySelector('.side-bar');
+
+addButton.addEventListener('click', toggleBurger);
+
+function toggleBurger(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if (!e.currentTarget) return;
+  if (e.currentTarget) {
+    addButton.classList.toggle('is-active');
+    sideBar.classList.toggle('expanded');
+  }
+}
