4d59feba852dc6a5b8bf4031b42eccd1eb098fac
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-09-26 01:54:31 +0300

    localStorage in progress

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,38 +1,77 @@
-import Glide from '@glidejs/glide';
+// import Glide from '@glidejs/glide';
+
+import items from '../../services/ITEMS.json';
+import ratingMarkup from './templates/rating.hbs';
+import colorsMarkup from './templates/colors.hbs'; //!
+// import detailsMarkup from './templates/details.hbs';
+// import helpers from '../../services/helpers';
 
 import '../../scss/components/card.scss';
 import '../../scss/components/sidebar.scss';
+import helpers from '../../services/helpers';
 
+// json data
+const data = items.ITEMS[0];
+console.log(data);
+
+let quantity = 1;
+let priceValue = Number(data.PRICE.slice(1));
 // ================= QUERY SELECTOR =================
 // side bar
-const addToCartBtn = document.querySelector('.js-btn-add');
-const sideBar = document.querySelector('.js-cart');
+const cartIconMain = document.querySelector('.js-cart-icon');
+const cartIconHeading = document.querySelector('.js-heading-cart');
+const addToCartBtn = document.querySelector('.js-btn-add'); //! not used
+const sidebarUl = document.querySelector('.sidebar__list'); //! not used
+const sideBar = document.querySelector('.js-sidebar');
 const overlay = document.querySelector('.js-overlay');
 // quantity
 const counter = document.querySelector('.js-number');
-const price = document.querySelector('.js-amount');
+const amount = document.querySelector('.js-amount');
 const increment = document.querySelector("[data-action='increment']");
 const decrement = document.querySelector("[data-action='decrement']");
+// markup
+const details = document.querySelector('.js-details');
+const rating = document.querySelector('.js-rating'); // before end
+const colors = document.querySelector('.js-colors'); //! not used
+const price = document.querySelector('.js-amount');
 
 // ================= ADD/REMOVE LISTENER =================
 // add listener
+addListener();
 function addListener() {
-  addToCartBtn.addEventListener('click', openCart);
+  addToCartBtn.addEventListener('click', pressAddBtn);
+  cartIconHeading.addEventListener('click', headingCart);
+  cartIconMain.addEventListener('click', openCart);
   overlay.addEventListener('click', closeCart);
   increment.addEventListener('click', increase);
   decrement.addEventListener('click', decrease);
 }
-addListener();
 // remove listener
 function removeListener() {
-  addToCartBtn.removeEventListener('click', openCart);
+  cartIconMain.removeEventListener('click', openCart);
   overlay.removeEventListener('click', closeCart);
   window.removeEventListener('keydown', closeByPressEsc);
 }
+// ================= ADD TO CART BUTTON =================
+//! add button
+function pressAddBtn() {
+  helpers.save('json', data);
+}
+
 // ================= OPEN/CLOSE CART =================
 // open cart
+function headingCart(e) {
+  e.stopPropagation();
+
+  if (!e.currentTarget) return;
+  if (e.currentTarget) {
+    sideBar.classList.add('expanded');
+    overlay.classList.add('overlay');
+    window.addEventListener('keydown', closeByPressEsc);
+  }
+}
+// open cart
 function openCart(e) {
-  e.preventDefault();
   e.stopPropagation();
 
   if (!e.currentTarget) return;
@@ -54,7 +93,7 @@ function closeByPressEsc(e) {
     removeClass();
   }
 }
-// close on close button
+//! close on close button
 function closeOnButtonClick(e) {
   if (e.target.nodeName === 'BUTTON') {
     removeClass();
@@ -68,9 +107,6 @@ function removeClass() {
 }
 
 // ================= QUANTITY/PRICE =================
-let quantity = 1;
-let amount = 499.95;
-
 // increase quantity +1
 function increase() {
   quantity += 1;
@@ -97,9 +133,9 @@ function decrease() {
 }
 // price per quantity
 function quantityPrice() {
-  const result = quantity * amount;
-  const rounded = result.toFixed(2);
-  price.innerHTML = `$${rounded}`;
+  const result = quantity * priceValue;
+  // const rounded = result.toFixed(2);
+  amount.innerHTML = `₴${result}`;
 }
 
 // ================= SLIDER =================
@@ -126,3 +162,25 @@ function quantityPrice() {
 //     glide.mount();
 //   }
 // }
+
+// ================= MARKUP =================
+
+const capitalizeWord =
+  data.DESCRIPTION.charAt(0).toUpperCase() + data.DESCRIPTION.slice(1);
+
+let cropped = data.NAME;
+let lastIndex = cropped.lastIndexOf(' ');
+cropped = cropped.substring(0, lastIndex);
+
+function detailsMarkup() {
+  return `
+    <span class="details__model">Model:${data.ID}</span>
+    <h3 class="details__name">${cropped}</h3>
+    <p class="details__about">${capitalizeWord}</p>`;
+}
+// details
+details.innerHTML = detailsMarkup();
+// price
+price.innerHTML = `₴ ${priceValue}`;
+// rating
+rating.insertAdjacentHTML('beforeend', ratingMarkup(data));
